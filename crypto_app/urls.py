f7679db23e004376efc91a51bbdf92626400ff9a
joe-alfaro <josephlalfaro@gmail.com>
2017-12-26 11:12:42 -0800

    deleting extra app in project and making changes for docker

--- crypto_app/urls.py
+++ crypto_app/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^cryptoApp/', include('cryptoApp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
